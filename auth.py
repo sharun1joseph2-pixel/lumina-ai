4292d9715ddcd1469da1bcb058848cd9dc4076ac
Shaarun Joseph <sharun1joseph2@gmail.com>
2026-05-22 22:11:11 +0530

    Add Lumina AI project files

--- auth.py
+++ auth.py
@@ -6,16 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
 
+if not SECRET_KEY:
+    raise Exception("JWT_SECRET_KEY missing in .env")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def create_token(data: dict):
-    return jwt.encode(data, OPENAI_API_KEY, algorithm=ALGORITHM)
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
